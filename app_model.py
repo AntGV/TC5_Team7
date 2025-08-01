e9e57be5ea8620224732fb4c78a8832ec7465e01
AntGV <angarval@hotmail.es>
2025-08-01 11:32:39 +0200

    Team Challenge 5

---
+++ app_model.py
@@ -0,0 +1,63 @@
+from flask import Flask, jsonify, request
+import numpy as np
+import os
+import pandas as pd
+import pickle
+
+
+# os.chdir(os.path.dirname(__file__))
+app = Flask(__name__)
+
+
+# Enruta la landing page (endpoint /)
+@app.route("/", methods = ["GET"])
+def hello(): # Ligado al endopoint "/" o sea el home, con el método GET
+    texto = "Bienvenido a la API predictora del tiempo desarrollada por el Team-7" + "\n" + "\n" + \
+        "Las variables necesarias para la predicción son:" + "\n" + \
+            "temp= hum= winds= prec= cloudc= atmp= uvi= season= vis= loc=" + "\n" + \
+            "temp (Temperatura - Temperature // float, medido en grados Celsius)" + "\n" + \
+            "hum (Humedad - Humidity // int, en porcentaje de humedad)" + "\n" + \
+            "winds (Velocidad del viento - Wind Speed // float, en kilómetros por hora)" + "\n" + \
+            "prec (Precipitaciones - Precipitation // float, en porcentaje de precipitaciones)" + "\n" + \
+            "cloudc (Cobertura de nubes - Cloud Cover // str [clear, partly cloudy, cloudy, overcast])" + "\n" + \
+            "atmp (Presión atmosférica - Atmospheric Pressure // float, en milibares)" + "\n" + \
+            "uvi (Índice UV - UV Index // int, con valores entre 0 y 14)" + "\n" + \
+            "season (Estación - Season // str, [Spring, Summer, Autumn, Winter])" + "\n" + \
+            "vis (Visibilidad - Visibility // float, rango de visibilidad en kilómetros)" + "\n" + \
+            "loc (Ubicación - Location // str [coastal, inland, mountain])" + "\n"
+
+    return texto
+
+# Enruta la funcion al endpoint /api/v1/predict
+@app.route("/api/v1/predict", methods = ["GET"])
+def predict(): # Ligado al endpoint '/api/v1/predict', con el método GET
+    with open("modelo_clasificador.pkl", "rb") as f:
+        model = pickle.load(f)
+
+    temp = request.args.get("Temperature", None)
+    hum = request.args.get("Humidity", None)
+    winds = request.args.get("Wind_Speed", None)
+    prec = request.args.get("Precipitation", None)
+    cloudc = request.args.get("Cloud_Cover", None)
+    atmp = request.args.get("Atm_Press", None)
+    uvi = request.args.get("UV_I", None)
+    season = request.args.get("Season", None)
+    vis = request.args.get("Visibility", None)
+    loc = request.args.get("Location", None)
+
+    prediction = model.predict([[float(temp),float(hum),float(winds),float(prec),float(cloudc),float(atmp),float(uvi),float(season),float(vis),float(loc)]])
+    #return jsonify({'predictions': prediction[0]})
+    if jsonify({'predictions': prediction[0]}) == 0:
+        etiqueta = "Sunny, thank you for the sunshine bouquet"
+    elif jsonify({'predictions': prediction[0]}) == 1:
+        etiqueta = "Cloudy, the sky is gray and white and cloudy"
+    elif jsonify({'predictions': prediction[0]}) == 2:
+        etiqueta = "On and on the rain will fall"
+    elif jsonify({'predictions': prediction[0]}) == 2:
+        etiqueta = "Let it snow!, Let it snow!, Let it snow!"
+    
+    return etiqueta
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
